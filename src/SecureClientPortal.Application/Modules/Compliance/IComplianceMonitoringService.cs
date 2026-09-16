2bfc5eff054d9b596313ee069ca8fcf930058a53
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-09-16 16:28:36 +0200

    Expose CIPC authority verification service

--- src/SecureClientPortal.Application/Modules/Compliance/IComplianceMonitoringService.cs
+++ src/SecureClientPortal.Application/Modules/Compliance/IComplianceMonitoringService.cs
@@ -9,5 +9,6 @@ public interface IComplianceMonitoringService
     Task<ServiceResult<ComplianceMonitoringResponse>> GetAsync(Guid clientId, ClaimsPrincipal user, CancellationToken ct);
     Task<ServiceResult<ComplianceMonitoringResponse>> UpdateAsync(Guid clientId, UpdateComplianceMonitoringRequest request, ClaimsPrincipal user, CancellationToken ct);
     Task<ServiceResult<ComplianceMonitoringResponse>> RecordManualAsync(Guid clientId, RecordManualVerificationRequest request, ClaimsPrincipal user, CancellationToken ct);
+    Task<ServiceResult<ComplianceMonitoringResponse>> VerifyCipcAsync(Guid clientId, RunAuthorityVerificationRequest request, ClaimsPrincipal user, CancellationToken ct);
     Task<ServiceResult<IReadOnlyList<VerificationResponse>>> HistoryAsync(Guid clientId, string? checkCode, int page, ClaimsPrincipal user, CancellationToken ct);
 }
